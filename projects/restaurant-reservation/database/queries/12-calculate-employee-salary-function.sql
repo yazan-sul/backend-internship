38cdf39c12efa8a9d3e117423ab025e19f0a2d66
yazan-sul <yazan5sulaiman@gmail.com>
2026-09-24 17:46:29 +0300

    -

--- projects/restaurant-reservation/database/queries/12-calculate-employee-salary-function.sql
+++ projects/restaurant-reservation/database/queries/12-calculate-employee-salary-function.sql
@@ -1,36 +1,20 @@
-CREATE OR REPLACE FUNCTION sp_ResrvedTablesReport(StartDate date, EndDate date)
-  RETURNS TABLE (
-    table_id integer,
-    table_number integer,
-    reserved_date date
-  )
+ CREATE OR REPLACE FUNCTION fn_CalculateEmployeeSalary(EmployeeId integer)
+  RETURNS bigint
   LANGUAGE sql
   AS $$
-    SELECT
-      restaurant_table.table_id,
-      restaurant_table.table_number,
-      reservation.reservation_date
-    FROM
-      reservations AS reservation
-      JOIN restaurant_tables AS restaurant_table ON restaurant_table.table_id = reservation.table_id
-    WHERE
-      reservation.reservation_date BETWEEN StartDate AND EndDate;
+      SELECT
+          COUNT(restaurant_order.order_id) *
+          CASE employee.position
+              WHEN 'VIPOrdersWaiter' THEN 5
+              WHEN 'StandardWaiter' THEN 4
+              WHEN 'AssistantWaiter' THEN 3
+              ELSE 0
+          END
+      FROM employees AS employee
+      LEFT JOIN orders AS restaurant_order
+          ON restaurant_order.employee_id = employee.employee_id
+      WHERE employee.employee_id = EmployeeId
+      GROUP BY
+          employee.employee_id,
+          employee.position;
   $$;
-
-SELECT
-    employee.employee_id,
-    employee.position,
-    COUNT(restaurant_order.order_id) AS order_count,
-    CASE employee.position
-        WHEN 'VIPOrdersWaiter' THEN 5
-        WHEN 'StandardWaiter' THEN 4
-        WHEN 'AssistantWaiter' THEN 3
-        ELSE 0
-    END AS position_rank
-FROM employees AS employee
-LEFT JOIN orders AS restaurant_order
-    ON restaurant_order.employee_id = employee.employee_id
-WHERE employee.employee_id = 2
-GROUP BY
-    employee.employee_id,
-    employee.position;
